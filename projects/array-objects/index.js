a1591ec47d4a97fd5cebb0e812335c26fde82250
Maria Pshenichnaya <psh.maria@mail.ru>
2023-05-03 17:33:16 +0500

    The second homework is done

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,13 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el)); // выведет каждый элемент массива
  */
-function forEach() {}
+
+function forEach(array, fn) {
+  for (const [ix, el] of array.entries()) {
+    fn(el, ix, array);
+  }
+}
+forEach([1, 2, 3], (el) => console.log(el));
 
 /*
  Задание 2:
@@ -21,7 +27,18 @@ function forEach() {}
    const newArray = map([1, 2, 3], (el) => el ** 2);
    console.log(newArray); // выведет [1, 4, 9]
  */
-function map() {}
+
+function map(array, fn) {
+  const newArray = [];
+
+  for (const [ix, el] of array.entries()) {
+    newArray.push(fn(el, ix, array));
+  }
+
+  return newArray;
+}
+
+map([1, 2, 3], (el) => console.log(el ** 2));
 
 /*
  Задание 3:
@@ -33,7 +50,22 @@ function map() {}
    const sum = reduce([1, 2, 3], (all, current) => all + current);
    console.log(sum); // выведет 6
  */
-function reduce() {}
+function reduce(array, fn, initial) {
+  let startIndex = 0;
+  let all = initial;
+
+  if (initial === undefined) {
+    startIndex = 1;
+    all = array[0];
+  }
+
+  for (let i = startIndex; i < array.length; i++) {
+    all = fn(all, array[i], i, array);
+  }
+  return all;
+}
+
+reduce([1, 2, 3], (all, current) => all + current);
 
 /*
  Задание 4:
@@ -44,6 +76,15 @@ function reduce() {}
    const keys = upperProps({ name: 'Сергей', lastName: 'Петров' });
    console.log(keys) // выведет ['NAME', 'LASTNAME']
  */
-function upperProps() {}
+const person = {
+  name: 'Сергей',
+  lastName: 'Петров',
+};
+
+function upperProps(object) {
+  return Object.keys(object).map((key) => key.toUpperCase());
+}
+
+upperProps(person);
 
 export { forEach, map, reduce, upperProps };
